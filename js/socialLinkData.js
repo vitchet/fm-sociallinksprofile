ba28bcd1117910d51088fd340d4a11b96bebb9c2
vitchet <94477735+vitchet@users.noreply.github.com>
2025-03-16 10:38:57 +0300

    Added js folder and the data file for the profile component.

---
+++ js/socialLinkData.js
@@ -0,0 +1,7 @@
+export const socialLinkData = {
+    name: "Jessica Randall",
+    heroImage: "avatar-jessica.jpeg",
+    location: "London, United Kingdom",
+    occupation: "Front-end developer and avid reader.",
+    links: ["GitHub", "Frontend Mentor", "LinkedIn", "Twitter", "Instagram"]
+};
